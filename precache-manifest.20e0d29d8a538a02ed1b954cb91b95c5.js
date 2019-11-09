d0985cf97561e79738662e40f130dbedcbe9333e
Elena Kolesnikova <elenakolesnikova@Elenas-MacBook-Pro.local>
2019-11-09 16:37:15 +0100

    Updates

---
+++ precache-manifest.20e0d29d8a538a02ed1b954cb91b95c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486fd8535c14776cc15a5707a38344e4",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "6a604f7aabf4e3e211d2",
+    "url": "/burger-builder/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c6dc482587da9e412eea",
+    "url": "/burger-builder/static/js/2.2b45dfae.chunk.js"
+  },
+  {
+    "revision": "6a604f7aabf4e3e211d2",
+    "url": "/burger-builder/static/js/main.6764f3e6.chunk.js"
+  },
+  {
+    "revision": "01f389611d85dd339f28",
+    "url": "/burger-builder/static/js/runtime-main.fff5d8f9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/burger-builder/static/media/logo.25bf045c.svg"
+  }
+]);
